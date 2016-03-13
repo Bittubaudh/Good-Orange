360b7db3cbfd25f7c0593a9442df0614abc14eef
Garrett Nickel <gmnicke2@illinois.edu>
2016-03-13 16:39:30 -0500

    trying out postgres

--- scripts/db.js
+++ scripts/db.js
@@ -1,24 +1,9 @@
-var mysql = require("mysql");
+var pg = require("pg");
+
 // Connect to database
-/*
-var connection = mysql.createConnection({
-  host: "hostname",
-  user: "db_user",
-  pw: "db_pw"
-});
 
-// On connection
-connection.connect(function(err) {
-  if(err) {
-  	console.log("Error connecting to database");
-  	return;
-  }
-  console.log("Connection established");
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if(err) throw err;
+  console.log("Connected to DB, getting schemas...");
 });
-
-// On connection termination
-connection.end(function(err) {
-  // 'Gracefully' terminate, wait until enqueued queries
-  // are good before COM_QUIT @ the MySQL server
-});
-*/
