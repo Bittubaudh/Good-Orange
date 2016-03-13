fffb8aa56f6cab86a49f0592cb20ce39fa83392a
Garrett Nickel <gmnicke2@illinois.edu>
2016-03-13 15:45:37 -0500

    Finished skeleton for master branch

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
 
+// Use HTML files from /view directory
+app.use(express.static(__dirname+"/view"));
+
+// Use CSS and JS files from /scripts directory
+app.use(express.static(__dirname+"/scripts"));
+
+
 app.get('/', function(req, res) {
   res.send('Good Orange');
   //TODO: sendFile with path to route actual HTML files
