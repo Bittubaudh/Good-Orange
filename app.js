fe30895ade521248621b0f1c41972c9efda59235
raghavnarain3 <raghavnarain3@gmail.com>
2016-03-15 21:44:47 -0500

    added db.js

--- app.js
+++ app.js
@@ -19,17 +19,7 @@ app.listen(port, function() {
   console.log('App listening');
 });
 
-var pg = require("pg");
-// Connect to database
+var db = require('./db');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if(err) throw err;
-  console.log("Connected to DB, getting schemas...");
-
-  var ret = client
-    .query("SELECT * FROM restaurant");
-
-  console.log(JSON.stringify(ret));
-});
+db.connect();
 
