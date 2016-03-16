4f5a2b2a74b6eab6edcd6ef13c4a4e7fbb2092c7
raghavnarain3 <raghavnarain3@gmail.com>
2016-03-15 23:50:19 -0500

    trying to add restaurant

--- db.js
+++ db.js
@@ -17,5 +17,36 @@ module.exports = {
           cb(results);
         });
     });
+  },
+
+addRestaurant: function(req, pg, cb) {
+    var result = {
+      'status': 'ERROR',
+      'message': 'Error connecting',
+      'contents': {}
+    };
+
+    pg.connect("postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj", 
+      function(err, client, done) {
+      if(err) {
+        done();
+        result['contents'] = {'errorMSG': err};
+        cb(result);
+      }
+      else {
+        console.log("Connected to DB, getting schemas...");
+        var request = req.params;
+
+        client
+          .query("INSERT INTO restaurant VALUES ('" + request.name + "', '" + request.location + "', " + request.qualityrating + ", " + request.pricerating + ", '" + request.foodstyle + "' );")
+          .on('end', function() {
+            done();
+            result['status'] = 'SUCCESS';
+            result['message'] = 'Restaurant ' + request.name + ' successfully added.';
+            result['contents'] = request;
+            cb(results);
+          });
+      }
+    });
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 module.exports = function(app, pg) {
+  var handleResponse = function(response) {
+    return res.json(response);
+  }
+
   var db = require('../db');
-	app.get('/api/v1/restaurants', function(req, res) {
-    db.getAllRestaurants(pg, function(response) {
-      return res.json(response);
-    });
-	});
+  app.get('/api/v1/restaurants', function(req, res) {
+      db.getAllRestaurants(pg, handleResponse);
+  });
+  app.post('/api/v1/restaurants', function(req, res) {
+  	db.addRestaurant(req, pg, handleResponse);
+  });
+
 }
