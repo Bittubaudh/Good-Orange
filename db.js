6ab20562bf7bf0db45a681dd21737739977dc5d4
gmnicke2 <gmnicke2@illinois.edu>
2016-03-15 23:57:32 -0500

    Post restaurants still broken

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 module.exports = {
-  getAllRestaurants: function(pg, cb) {
+  getAllRestaurants: function(pg, res, cb) {
     var results = [];
 
-    pg.connect(process.env.DATABASE_URL, 
+    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
       function(err, client, done) {
       if(err) {done(); console.log(err);}
       console.log("Connected to DB, getting schemas...");
@@ -14,12 +14,12 @@ module.exports = {
         })
         .on('end', function() {
           done();
-          cb(results);
+          cb(results, res);
         });
     });
   },
 
-addRestaurant: function(req, pg, cb) {
+addRestaurant: function(req, pg, res, cb) {
     var result = {
       'status': 'ERROR',
       'message': 'Error connecting',
@@ -31,7 +31,7 @@ addRestaurant: function(req, pg, cb) {
       if(err) {
         done();
         result['contents'] = {'errorMSG': err};
-        cb(result);
+        cb(result, res);
       }
       else {
         console.log("Connected to DB, getting schemas...");
@@ -44,7 +44,7 @@ addRestaurant: function(req, pg, cb) {
             result['status'] = 'SUCCESS';
             result['message'] = 'Restaurant ' + request.name + ' successfully added.';
             result['contents'] = request;
-            cb(results);
+            cb(results, res);
           });
       }
     });
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 module.exports = function(app, pg) {
-  var handleResponse = function(response) {
+  var handleResponse = function(response, res) {
     return res.json(response);
   }
 
   var db = require('../db');
   app.get('/api/v1/restaurants', function(req, res) {
-      db.getAllRestaurants(pg, handleResponse);
+      db.getAllRestaurants(pg, res, handleResponse);
   });
   app.post('/api/v1/restaurants', function(req, res) {
-  	db.addRestaurant(req, pg, handleResponse);
+  	db.addRestaurant(req, pg, res, handleResponse);
   });
 
 }
