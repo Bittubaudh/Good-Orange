399ba0f6223615a673f97e4e68d85e336ba247cf
gmnicke2 <gmnicke2@illinois.edu>
2016-03-16 00:32:16 -0500

    made database url heroku's

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ module.exports = {
   getAllRestaurants: function(pg, res, cb) {
     var results = [];
 
-    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {done(); console.log(err);}
       console.log("Connected to DB, getting schemas...");
@@ -26,7 +26,7 @@ addRestaurant: function(req, pg, res, cb) {
       'contents': {}
     };
 
-    pg.connect("postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj", 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {
         done();
