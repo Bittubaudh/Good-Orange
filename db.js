f43aa0be586be0997aa01107c80deecd83020679
gmnicke2 <gmnicke2@illinois.edu>
2016-03-15 21:31:16 -0500

    deleted db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-var pg = require("pg");
-// Connect to database
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-if(err) throw err;
-console.log("Connected to DB, getting schemas...");
-});
