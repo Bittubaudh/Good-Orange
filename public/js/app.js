1f3a72da9873231306eaee971e4c1d357fea8b5a
achiu17 <achiu17@gmail.com>
2016-03-17 13:32:34 -0500

    bugs with front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,43 @@
-var app=angular.module("restaurantModule",["ngRoute","movieControllers"]);app.config(["$routeProvider",function($routeProvider){$routeProvider.when("/home",{templateUrl:"partials/home.html",compile:function(){$(document).foundation()},controller:"homeCtrl"}).when("/search",{templateUrl:"partials/search.html",controller:"searchCtrl"}).when("/reviews",{templateUrl:"partials/reviews.html",controller:"reviewsCtrl"}).when("/login",{templateUrl:"partials/login.html",controller:"loginCtrl"}).when("/newuser",{templateUrl:"partials/newuser.html",controller:"newuserCtrl"}).when("/addRestaurant",{templateUrl:"partials/addRestaurant.html",controller:"addRestaurantCtrl"}).when("/viewRestaurants",{templateUrl:"partials/viewRestaurants.html",controller:"viewRestaurantsCtrl"}).otherwise({redirectTo:"/home"})}]);
+var app=angular.module("restaurantModule",["ngRoute", 'movieControllers']);
+app.config(['$routeProvider',
+    function($routeProvider) {
+    $routeProvider.
+    when('/home', {
+        templateUrl: 'partials/home.html',
+        compile: function() {
+  $(document).foundation();
+},
+        controller: 'homeCtrl'
+    }).
+    when('/search', {
+        templateUrl: 'partials/search.html',
+        controller: 'searchCtrl'
+    }).
+    when('/reviews', {
+        templateUrl: 'partials/reviews.html',
+        controller: 'reviewCtrl'
+    }).
+    when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'loginCtrl'
+    }).
+    when('/newuser', {
+        templateUrl: 'partials/newuser.html',
+        controller: 'newuserCtrl'
+    }).
+    when('/addRestaurant', {
+        templateUrl: 'partials/addRestaurant.html',
+        controller: 'addRestaurantCtrl'
+    }).
+    when('/viewRestaurants', {
+        templateUrl: 'partials/viewRestaurants.html',
+        controller: 'viewRestaurantsCtrl'
+    }).
+    when('/deleteRestaurants', {
+        templateUrl: 'partials/deleteRestaurants.html',
+        controller: 'deleteRestaurantsCtrl'
+    }).
+    otherwise({
+        redirectTo: '/home'
+    });
+}]);
