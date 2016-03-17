105612d011acc7a79f9157c8542bf705a766846b
gmnicke2 <gmnicke2@illinois.edu>
2016-03-16 21:40:25 -0500

    Added more routes for other db's

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ module.exports = {
   getAllRestaurants: function(pg, res, cb) {
     var results = [];
 
-    pg.connect(process.env.DATABASE_URL, 
+    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
       function(err, client, done) {
       if(err) {done(); console.log(err);}
       console.log("Connected to DB, getting schemas...");
@@ -26,7 +26,7 @@ addRestaurant: function(req, pg, res, cb) {
       'contents': {}
     };
 
-    pg.connect(process.env.DATABASE_URL, 
+    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
       function(err, client, done) {
       if(err) {
         done();
@@ -47,6 +47,145 @@ addRestaurant: function(req, pg, res, cb) {
             cb(result, res);
           });
       }
+    });
+  },
+
+  addCustomer: function(req, pg, res, cb) {
+    var result = {
+      'status': 'ERROR',
+      'message': 'Error connecting',
+      'contents': {}
+    };
+
+    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+      function(err, client, done) {
+      if(err) {
+        done();
+        result['contents'] = {'errorMSG': err};
+        cb(result, res);
+      }
+      else {
+        console.log("Connected to DB, getting schemas...");
+        var request = req.body;
+
+        var queryString = "INSERT INTO customer VALUES ('" + request.fullname + "', '" + request.username + "', '" + request.hashedpassword + "', " + request.zipcode + ", ";
+        var foodStyles = request['favoritefoodstyles[]'];
+        var insertArrayStr = "";
+        if(foodStyles.length > 0) {
+          insertArrayStr = "'{";
+          for(var i = 0; i < foodStyles.length-1; i++) {
+            insertArrayStr += '"'+foodStyles[i]+'", ';
+          }
+          insertArrayStr += '"'+foodStyles[foodStyles.length-1]+'"}\''
+        } else {
+          insertArrayStr = "'{}'"
+        }
+
+        queryString += insertArrayStr + ');';
+
+        client
+          .query(queryString)
+          .on('end', function() {
+            done();
+            result['status'] = 'SUCCESS';
+            result['message'] = 'Customer account for ' + request.fullname + ' (' + request.username + ') successfully added.';
+            result['contents'] = request;
+            cb(result, res);
+          });
+      }
+    });
+  },
+
+  getCustomerByUN: function(req, pg, res, cb) {
+    var results = [];
+
+    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+      function(err, client, done) {
+      if(err) {done(); console.log(err);}
+      console.log("Connected to DB, getting schemas...");
+
+      client
+        .query("SELECT * FROM customer WHERE username='"+req.params.username+"';")
+        .on('row', function(row) {
+          results.push(row);
+        })
+        .on('end', function() {
+          done();
+          cb(results[0], res);
+        });
+    });
+  },
+
+  getReviewByLocation: function(req, pg, res, cb) {
+    var results = [];
+
+    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+      function(err, client, done) {
+      if(err) {done(); console.log(err);}
+      console.log("Connected to DB, getting schemas...");
+
+      client
+        .query("SELECT * FROM review WHERE location='"+req.params.location+"';")
+        .on('row', function(row) {
+          results.push(row);
+        })
+        .on('end', function() {
+          done();
+          cb(results[0], res);
+        });
+    });
+  },
+
+  addReview: function(req, pg, res, cb) {
+    var result = {
+      'status': 'ERROR',
+      'message': 'Error connecting',
+      'contents': {}
+    };
+
+    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+      function(err, client, done) {
+      if(err) {
+        done();
+        result['contents'] = {'errorMSG': err};
+        cb(result, res);
+      }
+      else {
+        console.log("Connected to DB, getting schemas...");
+        var request = req.body;
+
+        client
+          .query("INSERT INTO review VALUES (now(), " + request.qualityrating + ", '" + 
+            request.comment + "', " + request.pricerating + ", '" + request.username+ "', '" + request.location + "', '" +
+            request.restaurantname + "');")
+          .on('end', function() {
+            done();
+            result['status'] = 'SUCCESS';
+            result['message'] = 'Review for ' + request.restaurantname + ', by ' + request.username + ', successfully added.';
+            result['contents'] = request;
+            cb(result, res);
+          });
+      }
+    });
+  },
+
+  getAllReviews: function(pg, res, cb) {
+    var results = [];
+
+    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+      function(err, client, done) {
+      if(err) {done(); console.log(err);}
+      console.log("Connected to DB, getting schemas...");
+
+      client
+        .query("SELECT * FROM review;")
+        .on('row', function(row) {
+          results.push(row);
+        })
+        .on('end', function() {
+          done();
+          cb(results, res);
+        });
     });
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ module.exports = function(app, pg) {
   }
 
   var db = require('../db');
+  // Restaurant endpoints
   app.get('/api/v1/restaurants', function(req, res) {
       db.getAllRestaurants(pg, res, handleResponse);
   });
@@ -11,4 +12,22 @@ module.exports = function(app, pg) {
   	db.addRestaurant(req, pg, res, handleResponse);
   });
 
+  // Customer endpoints
+  app.get('/api/v1/customers/:username', function(req, res) {
+      db.getCustomerByUN(req, pg, res, handleResponse);
+  });
+  app.post('/api/v1/customers', function(req, res) {
+    db.addCustomer(req, pg, res, handleResponse);
+  });
+
+  // Review endpoints
+  app.get('/api/v1/reviews', function(req, res) {
+      db.getAllReviews(pg, res, handleResponse);
+  });
+  app.get('/api/v1/reviews/location/:location', function(req, res) {
+      db.getReviewByLocation(req, pg, res, handleResponse);
+  });
+  app.post('/api/v1/reviews', function(req, res) {
+    db.addReview(req, pg, res, handleResponse);
+  });
 }
