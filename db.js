c518f1c2299cc762cb3f53d430ad7f22e63061a7
gmnicke2 <gmnicke2@illinois.edu>
2016-03-17 14:25:44 -0500

    re deploying through git trigger

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ module.exports = {
     pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {done(); console.log(err);}
-      console.log("Connected to DB, getting schemas...");
+      console.log("Connected to DB, getting schemas....");
 
       client
         .query("SELECT * FROM restaurant;")
