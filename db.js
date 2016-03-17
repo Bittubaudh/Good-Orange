6fcaba43e5adfcc2457042142febe307d0fefc20
gmnicke2 <gmnicke2@illinois.edu>
2016-03-16 21:46:25 -0500

    Changed db url to process env

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ module.exports = {
   getAllRestaurants: function(pg, res, cb) {
     var results = [];
 
-    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {done(); console.log(err);}
       console.log("Connected to DB, getting schemas...");
@@ -26,7 +26,7 @@ addRestaurant: function(req, pg, res, cb) {
       'contents': {}
     };
 
-    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {
         done();
@@ -57,7 +57,7 @@ addRestaurant: function(req, pg, res, cb) {
       'contents': {}
     };
 
-    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {
         done();
@@ -99,7 +99,7 @@ addRestaurant: function(req, pg, res, cb) {
   getCustomerByUN: function(req, pg, res, cb) {
     var results = [];
 
-    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {done(); console.log(err);}
       console.log("Connected to DB, getting schemas...");
@@ -119,7 +119,7 @@ addRestaurant: function(req, pg, res, cb) {
   getReviewByLocation: function(req, pg, res, cb) {
     var results = [];
 
-    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {done(); console.log(err);}
       console.log("Connected to DB, getting schemas...");
@@ -143,7 +143,7 @@ addRestaurant: function(req, pg, res, cb) {
       'contents': {}
     };
 
-    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {
         done();
@@ -172,7 +172,7 @@ addRestaurant: function(req, pg, res, cb) {
   getAllReviews: function(pg, res, cb) {
     var results = [];
 
-    pg.connect('postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj', 
+    pg.connect(process.env.DATABASE_URL, 
       function(err, client, done) {
       if(err) {done(); console.log(err);}
       console.log("Connected to DB, getting schemas...");
