a631afde24c8ef45cd942021ebe53ab27b970132
raghavnarain3 <raghavnarain3@gmail.com>
2016-04-17 16:32:27 -0500

    created get reviews by username

--- db.js
+++ db.js
@@ -185,6 +185,25 @@ addRestaurant: function(req, pg, res, cb) {
     });
   },
 
+  getReviewByUN: function(req, pg, res, cb) {
+    var results = [];
+    pg.connect("postgres://vgokgwmllyuvta:Y8jxNsM8vZOTSxd-fMBfvlqrF2@ec2-54-235-152-114.compute-1.amazonaws.com:5432/d51ijnnak3emfj", 
+      function(err, client, done) {
+      if(err) {done(); console.log(err);}
+      console.log("Connected to DB, getting schemas...");
+
+      client
+        .query("SELECT * FROM review WHERE username='"+req.params.username+"';")
+        .on('row', function(row) {
+          results.push(row);
+        })
+        .on('end', function() {
+          done();
+          cb(results[0], res);
+        });
+    });
+  },
+
   getReviewByLocation: function(req, pg, res, cb) {
     var results = [];
 
