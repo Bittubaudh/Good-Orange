783fe3b266fcc0559d0776eb5b8e94a45059aec1
Garrett Nickel <gmnicke2@illinois.edu>
2016-04-18 17:23:16 -0500

    new advanced feature file

---
+++ adv.js
@@ -0,0 +1,9 @@
+module.exports = {
+  getRecommendationsByHistory: function(req, pg, res, cb) {
+    cb({"not implemented": "not implemented"}, res);
+  },
+
+  getRecommendationsByLocation: function(req, pg, res, cb) {
+    cb({"not implemented": "not implemented"}, res);
+  }
+}
